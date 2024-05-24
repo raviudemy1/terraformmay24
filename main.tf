49c5dfccfecf470813b3a643e9c95a77c394e215
Lakshmi Prasanna <dlprasannabe05@gmail.com>
2024-05-24 21:54:47 +0100

    state lock file

--- main.tf
+++ main.tf
@@ -1,15 +1,35 @@
 provider "aws" {
- region="eu-west-2"
+ region = "eu-west-2"
  }
-
- #resource "aws_eip" "ip" {
-  #instance = aws_instance.example.id
- #}
- resource "aws_instance" "example" {
-  ami = "ami-053a617c6207ecc7b"
-  instance_type = "t2.micro"
-  depends_on = [aws_s3_bucket.example]
+# create s3 bucket to store remote state file #
+resource "aws_s3_bucket" "example" {
+ bucket = "prasanna-s3-remotestate"
  }
- resource "aws_s3_bucket" "example" {
-  bucket="prasanna_s3bucket"
+# create a dynamodb table for locking the state file#
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+ name           = "terraform-state-lock-dynamo"
+ hash-key       = "LockID"
+ read_capacity  = 20
+ write_capacity = 20
+ attribute {
+  name = "LockID"
+  type = "S"
+  }
+  tags = {
+   Name = "DynamoDB Terraform state Lock Table"
+  }
  } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
