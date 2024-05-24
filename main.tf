53a98c7e01ee54eccc8897ddfbab8660caa5555e
Lakshmi Prasanna <dlprasannabe05@gmail.com>
2024-05-24 12:46:46 +0100

    Implicit dependencies

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
 provider "aws" {
  region="eu-west-2"
  }
-resource "aws_instance" "example" {
- ami = "ami-053a617c6207ecc7b"
- instance_type = "t2.micro"
+
+ resource "aws_eip" "ip" {
+  instance = aws_instance.example.id
  }
-resource "aws_s3_bucket" "example" {
- bucket="prasanna_s3bucket"
-} 
+ resource "aws_instance" "example" {
+  ami = "ami-053a617c6207ecc7b"
+  instance_type = "t2.micro"
+ }
+#resource "aws_s3_bucket" "example" {
+ #bucket="prasanna_s3bucket"
+#} 
