b602d9b5060f1029ab27ff6346d2628a555c5779
Lakshmi Prasanna <dlprasannabe05@gmail.com>
2024-05-24 16:45:50 +0100

    explicit dependency

--- main.tf
+++ main.tf
@@ -2,13 +2,14 @@ provider "aws" {
  region="eu-west-2"
  }
 
- resource "aws_eip" "ip" {
-  instance = aws_instance.example.id
- }
+ #resource "aws_eip" "ip" {
+  #instance = aws_instance.example.id
+ #}
  resource "aws_instance" "example" {
   ami = "ami-053a617c6207ecc7b"
   instance_type = "t2.micro"
+  depends_on = [aws_s3_bucket.example]
  }
-#resource "aws_s3_bucket" "example" {
- #bucket="prasanna_s3bucket"
-#} 
+ resource "aws_s3_bucket" "example" {
+  bucket="prasanna_s3bucket"
+ } 
