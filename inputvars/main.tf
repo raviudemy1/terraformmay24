94bb3792a28d0c3ce0fa293fde90985b97de4829
Lakshmi Prasanna <dlprasannabe05@gmail.com>
2024-05-27 23:03:41 +0100

    adding input variables

---
+++ inputvars/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+ region = ""eu-west-2"
+ }
+resource "aws_instance" "example1" {
+ ami           = var.amiid 
+ instance_type = var.type
+ }
+
+variable "amiid" {
+ default = "ami-053a617c6207ecc7b"
+ }
+
+variable "type" {
+ defualt = t2.micro
+ }
