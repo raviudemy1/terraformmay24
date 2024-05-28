5e654c0272d64fd4871ddb6b865b5444a6d79d96
Lakshmi Prasanna <dlprasannabe05@gmail.com>
2024-05-28 18:09:22 +0100

    adding a new resource using inputvars

--- inputvars/main.tf
+++ inputvars/main.tf
@@ -6,6 +6,10 @@ resource "aws_instance" "example1" {
  instance_type = var.type
  }
 
+resource "aws_instance" "example2" {
+ ami           = var.amiid
+ instance_type = var.type
+
 variable "amiid" {
  default = "ami-053a617c6207ecc7b"
  }
